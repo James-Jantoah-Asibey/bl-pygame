281c0a0af40fe593c835890c2d000bca78d2729d
Terrance Clark <tclark555@boyslatin.net>
2025-03-13 08:47:06 -0400

    took notes

--- module_2/shootingGame.py
+++ module_2/shootingGame.py
@@ -11,13 +11,32 @@ class Player():  #what the plater looks like
         self.image = pygame.Surface((50,30))
         self.image.fill((0,0,255))
         self.rect = self.image.get_rect()
-        self.rect.center = (400,550)
+        self.rect.center = (400, 550)
 
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -=5
+        if keys[pygame.K_RIGHT]:
+            self.rect.x +=5
 
+class Bullets():
+    def  __init__(self,x,y):
+        super(). __init__()
+        self.image = pygame.Surface((5, 10))
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center =(x,y)                
 
+    def update(self):
+        self.rect.y -=7
+        self.rect.bottom < 0
+        self.kill()
 
-
-
+class Enemy():
+    def __init__ (self):
+        super().__init(self)
+        self.image = pygame.Surface
 
 
 
